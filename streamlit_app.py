b46c900cdcd988ad35952fc23021a78665e4a06a
jrmapa06 <jrmapa@yahoo.com>
2024-11-11 07:57:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,5 +56,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!',icon="🍌")
 import requests
-fruityvice_response = requests.get("https://api.github.com", auth=('jrmapa', 'Kamasuki14'))
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
